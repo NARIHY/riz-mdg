7c1ecb3794aa942b8c88fa3b960f2dd62c6531d0
NARIHY <maheninarandrianarisoa@gmail.com>
2025-03-13 15:59:36 +0300

    Initialisation du project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface MenuItem {
+  name: string;
+  link: string;
+}
+
+const menuItems: MenuItem[] = [
+  { name: 'Accueil', link: '/' },
+  { name: 'À propos', link: '/about' },
+  { name: 'Contact', link: '/contact' }
+];
+
+const isMenuOpen = ref<boolean>(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Mon Menu</a>
+        <button class="navbar-toggler" type="button" @click="toggleMenu">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse" :class="{ show: isMenuOpen }">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li v-for="item in menuItems" :key="item.name" class="nav-item">
+              <a class="nav-link" :href="item.link">{{ item.name }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
